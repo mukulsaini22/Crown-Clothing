3ef943187b85c13f596d3f47a4f24280161b0e0c
Mukul Saini <65353419+mukulsaini22@users.noreply.github.com>
2020-10-19 01:31:00 +0530

    add signinpage

---
+++ src/pagecomponent/signinpage/signin-signup.page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Sign from "../../components/signin.component/signin.component";
+import SignIn from "../../components/signin.component/signin.component";
+import "./signin-signup.styles.scss";
+import Signup from "../../components/signup.component/signup.component";
+const Signpage = () => (
+
+
+  <div className="sign-in-and-sign-up ">
+    <SignIn />
+    <Signup />
+
+  </div>
+
+);
+export default Signpage;
